7b54327b9ea5aad2ab3a53ef80cc4c130df5032d
Tyler Ilunga <tylerilunga1@gmail.com>
2019-07-10 21:24:49 -0700

    cleanup

--- services/ServiceBoiler/src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  retrieve(req, res) {},
-  verify(req, res) {},
-  update(req, res) {},
-  reset(req, res) {},
-  snapshot(req, res) {},
-};
--- services/ServiceBoiler/src/server/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('../../app.js');
-const config = require('../config');
-app.listen(config.port, error => {
-  console.log(error ? error : `Listening on port ${config.port}`);
-});
